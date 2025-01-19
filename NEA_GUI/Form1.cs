27e983a7662a2b5da9e7a8270536e0daa56b535b
kutiyt <Franklintomr@gmail.com>
2025-01-19 17:31:41 +0000

    Began attempting to implement a server.

--- NEA_GUI/Form1.cs
+++ NEA_GUI/Form1.cs
@@ -1,3 +1,8 @@
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace NEA_GUI
 {
     public partial class Form1 : Form
@@ -5,6 +10,7 @@ namespace NEA_GUI
         public Form1()
         {
             InitializeComponent();
+            Server._Server();
         }
 
         private void Send_Click(object sender, EventArgs e)
@@ -12,15 +18,40 @@ namespace NEA_GUI
             string msg;
             msg = formatBox.Text;
             displayBox.AppendText(msg + Environment.NewLine);
-            formatBox.Text = string.Empty;
+            formatBox.Text = string.Empty; 
         }
-        private void displayBox_TextChanged(object sender, EventArgs e)
+        public void displayBox_TextChanged(object sender, EventArgs e)
         {
             int lineCount;
             lineCount = displayBox.GetLineFromCharIndex(displayBox.TextLength);
             lineCounter.Text = Convert.ToString(lineCount);  
         }
 
+    }
+
+    public class Server
+    {
+        public void _Server() 
+        {
+            TcpListener listener;
+            var port = 1200;
+            var serverIP = IPAddress.Parse("127.0.0.1");
+            byte[] buffer = new byte[512];  
+            listener = new TcpListener(serverIP,port);
+            string msg;
+
+            using TcpClient tcpClient = listener.AcceptTcpClient();
+            
+                var netStream = tcpClient.GetStream();
+            int streamLength;
+            while ((streamLength = netStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                msg = Encoding.UTF8.GetString(buffer,0,buffer.Length);
+                displayBox.Text = msg;
+            }  
+            
+
+        }
     }
 
 }
